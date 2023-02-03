6163b27fbf8f4bb57650fae3c66f9244a1795065
Wixom78666 <jswix4@gmail.com>
2023-02-02 21:10:43 -0600

    completed models and indexjs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
+// Tags belongToMaPny Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+})
 module.exports = {
   Product,
   Category,
